bf74e91acfd42b184b8c063094a0d763849311f3
AzureAD\MiguelFrejoMarañón <2021346@iesluisbraille.es>
2022-03-10 09:37:54 +0100

    version inicial

---
+++ esPrimo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo;
+
+/**
+ *
+ * @author MiguelFrejoMarañón
+ */
+public class esPrimo {
+    public static boolean esPrimo(int n){
+        int i=2;
+        while(i<n)
+        {
+            if(n%1==0)
+                return false;
+            i++;
+        }
+        return true;
+    }
+    
+}
